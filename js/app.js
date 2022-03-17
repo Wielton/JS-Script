2f5938ee3be59c75f461a69e914b70a853eb6e76
Wielton <grantwielgosz@gmail.com>
2022-03-16 22:05:59 -0600

    Made a slight variation to the first IF statement

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 var userAge = 19;
 var isSubscribed = true;
 
-if (!(userAge >= 18) && !(isSubscribed = true)) {
+if (!((userAge >= 18) && (isSubscribed = true))) {
     console.log('The user is under 18 and not subscribed.');
 } else if ((userAge >= 18) && !(isSubscribed = true)) {
     console.log('The user is 18 or older and is not subscribed.');
